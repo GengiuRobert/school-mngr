806985ae47b6db674b5283941eeac9c4d943c432
GengiuRobert <fncrobert3@gmail.com>
2025-03-20 00:56:13 +0200

    home component integrated

--- school-mngr/src/app/app.component.ts
+++ school-mngr/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from "../components/header/header.component";
 import { LoginComponent } from "../components/login/login.component";
 import { SignupComponent } from "../components/signup/signup.component";
+import { HomeComponent } from "../components/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent],
+  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
