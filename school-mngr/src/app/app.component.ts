be0440dd47fed35c2eec70faf567c52661439ba1
GengiuRobert <fncrobert3@gmail.com>
2025-03-20 00:23:22 +0200

    login and signup components created

--- school-mngr/src/app/app.component.ts
+++ school-mngr/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from "../components/header/header.component";
+import { LoginComponent } from "../components/login/login.component";
+import { SignupComponent } from "../components/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
