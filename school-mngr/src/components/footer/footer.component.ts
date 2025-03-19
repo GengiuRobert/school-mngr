756cb7b927dcf4427cc8b34ca8b1b6919462d0c8
GengiuRobert <fncrobert3@gmail.com>
2025-03-19 21:46:59 +0200

    footer component created

---
+++ school-mngr/src/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  sticky = true;
+  transparent = true;
+  footerText = '© NgRx-University'; 
+  
+}
