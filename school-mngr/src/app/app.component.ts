a48b64d1e885c32400f53661993b454380a4dfa2
GengiuRobert <fncrobert3@gmail.com>
2025-03-20 22:50:03 +0200

    folder restructe

--- school-mngr/src/app/app.component.ts
+++ school-mngr/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from '../components/footer/footer.component';
-import { HeaderComponent } from "../components/header/header.component";
-import { LoginComponent } from "../components/login/login.component";
-import { SignupComponent } from "../components/signup/signup.component";
-import { HomeComponent } from "../components/home/home.component";
+import { FooterComponent } from '../components/web-page-config/footer/footer.component';
+import { HeaderComponent } from "../components/web-page-config/header/header.component";
+import { LoginComponent } from "../components/register/login/login.component";
+import { SignupComponent } from "../components/register/signup/signup.component";
+import { HomeComponent } from '../components/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent, HomeComponent],
+  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
