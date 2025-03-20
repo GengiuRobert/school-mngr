54e27959f3fae46833b7571e07ef48fe2051d310
GengiuRobert <fncrobert3@gmail.com>
2025-03-20 23:34:34 +0200

    professor view created

--- school-mngr/src/app/app.component.ts
+++ school-mngr/src/app/app.component.ts
@@ -6,10 +6,11 @@ import { LoginComponent } from "../components/register/login/login.component";
 import { SignupComponent } from "../components/register/signup/signup.component";
 import { HomeComponent } from '../components/pages/home/home.component';
 import { AdminComponent } from "../components/views/admin/admin.component";
+import { ProfessorComponent } from "../components/views/professor/professor.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent, HomeComponent, AdminComponent],
+  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent, HomeComponent, AdminComponent, ProfessorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
