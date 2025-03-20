28894db60e9ef5eefd14d8e7f7732ce0d54a918e
GengiuRobert <fncrobert3@gmail.com>
2025-03-20 23:28:03 +0200

    admin view created

--- school-mngr/src/app/app.component.ts
+++ school-mngr/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { HeaderComponent } from "../components/web-page-config/header/header.com
 import { LoginComponent } from "../components/register/login/login.component";
 import { SignupComponent } from "../components/register/signup/signup.component";
 import { HomeComponent } from '../components/pages/home/home.component';
+import { AdminComponent } from "../components/views/admin/admin.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent,HomeComponent],
+  imports: [FooterComponent, HeaderComponent, LoginComponent, SignupComponent, HomeComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
