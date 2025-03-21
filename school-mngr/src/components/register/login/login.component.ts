23f46d38c6ef42387c5b5d840e6c00530faa3ca4
GengiuRobert <fncrobert3@gmail.com>
2025-03-22 00:55:03 +0200

    log in integrated

--- school-mngr/src/components/register/login/login.component.ts
+++ school-mngr/src/components/register/login/login.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
-
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  email = '';
+  password = '';
+  success: string | null = null;
+  message: string | null = null;
+
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  resetForm() {
+    this.email = '';
+    this.password = '';
+  }
+
+  onLogin(form: NgForm) {
+
+    this.success = '';
+    this.message = '';
+
+    if (!form.valid) {
+      return;
+    }
+
+    this.authService.logIn(this.email, this.password).subscribe({
+      next: (response) => {
+        this.success = "Logged in successfully!";
+        this.resetForm();
+        this.router.navigate(['/home']);
+        console.log(response);
+      },
+      error: (err) => {
+        this.message = err.message;
+      }
+    });
+
+  }
 }
