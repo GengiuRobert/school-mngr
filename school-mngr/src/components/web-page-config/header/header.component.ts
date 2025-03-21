320914d935f08940d126bfac19a3179fdfc0002a
GengiuRobert <fncrobert3@gmail.com>
2025-03-21 12:50:46 +0200

    enable routing on pages // error 404 page not found

--- school-mngr/src/components/web-page-config/header/header.component.ts
+++ school-mngr/src/components/web-page-config/header/header.component.ts
@@ -1,16 +1,27 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavButton } from '../../../models/navbutton.model';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgFor],
+  imports: [NgFor, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   sticky = true;
-  menu = ['Home', 'About Us', 'Services', 'Contact'];
-  menuOpen = true;  
+
+  menu: NavButton[] = [
+    { name: 'Home', url: '/home' },
+    { name: 'About Us', url: '/about' },
+    { name: 'Services', url: '/services' },
+    { name: 'Contact', url: '/contact' }
+  ];
+  menuOpen = true;
+
+  constructor(private router: Router) { }
+
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
   }
@@ -18,4 +29,9 @@ export class HeaderComponent {
   closeMenu(): void {
     this.menuOpen = false;
   }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
 }
