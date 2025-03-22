62420645cc066a64c273c57153e7e8f4f2d3bb3c
GengiuRobert <fncrobert3@gmail.com>
2025-03-22 14:11:20 +0200

    configured routes for role of the logged in user

--- school-mngr/src/app/app.routes.ts
+++ school-mngr/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from '../components/pages/home/home.component';
 import { LoginComponent } from '../components/register/login/login.component';
 import { SignupComponent } from '../components/register/signup/signup.component';
 import { ErrorComponent } from '../components/pages/error/error.component';
+import { AdminComponent } from '../components/views/admin/admin.component';
+import { StudentComponent } from '../components/views/student/student.component';
+import { ProfessorComponent } from '../components/views/professor/professor.component';
 
 export const routes: Routes = [
 
@@ -10,6 +13,9 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: 'student', component: StudentComponent },
+    { path: 'professor', component: ProfessorComponent },
     { path: '404', component: ErrorComponent },
     { path: '**', redirectTo: '/404' }
 
