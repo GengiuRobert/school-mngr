770b5e64df03ded56a2da02b4fb81a252733882b
GengiuRobert <fncrobert3@gmail.com>
2025-03-22 13:57:45 +0200

    crud operations for users

--- school-mngr/src/services/users.service.ts
+++ school-mngr/src/services/users.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { User } from "../models/user.model";
-import { doc, Firestore, setDoc } from "@angular/fire/firestore";
+import { doc, docSnapshots, Firestore, setDoc } from "@angular/fire/firestore";
+import { collection, deleteDoc, getDoc, getDocs, updateDoc } from "firebase/firestore";
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
     constructor(private firestore: Firestore) { }
 
-    addUser(user: User,role:string) {
+    addUser(user: User, role: string) {
         const userRef = doc(this.firestore, 'users', user.id);
         return setDoc(userRef, {
             email: user.email,
@@ -21,4 +22,59 @@ export class UserService {
             });
     }
 
+    deleteUser(userId: string) {
+        const userRef = doc(this.firestore, 'users', userId);
+        return deleteDoc(userRef).then(() => {
+            console.log("User deleted from Firestore!");
+
+        }).catch(error => {
+            console.error("Error deleting user from Firestore: ", error);
+
+        });
+    }
+
+    updateUser(userId: string, updatedData: Partial<User>) {
+        const userRef = doc(this.firestore, 'users', userId);
+        return updateDoc(userRef, updatedData)
+            .then(() => {
+                console.log("User updated in Firestore!");
+            })
+            .catch(error => {
+                console.error("Error updating user in Firestore: ", error);
+            });
+    }
+
+
+    getUser(userId: string) {
+        const userRef = doc(this.firestore, 'users', userId);
+        return getDoc(userRef)
+            .then(docSnapshot => {
+                if (docSnapshot.exists()) {
+                    const userData = docSnapshot.data();
+                    return { ...userData, id: docSnapshot.id };
+                } else {
+                    console.log("No user found with the given ID.");
+                    return null;
+                }
+            })
+            .catch(error => {
+                console.error("Error getting user from Firestore: ", error);
+                return null;
+            });
+    }
+
+    getAllUsers() {
+        const userRef = collection(this.firestore, 'users');
+        return getDocs(userRef).then(querySnapshot => {
+            const users: User[] = [];
+            querySnapshot.forEach(docSnapshot => {
+                const userData = docSnapshot.data();
+                users.push({ ...userData, id: docSnapshot.id } as User);
+            });
+            return users;
+        }).catch(error => {
+            console.error("Error getting users from Firestore: ", error);
+            return [];
+        });
+    }
 }
