e32d6ccc41e8f3ed0e82474a23cd91239c0396be
GengiuRobert <fncrobert3@gmail.com>
2025-03-24 21:13:34 +0200

    display name instead of email

--- school-mngr/src/environments/environment.ts
+++ school-mngr/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyB3xJMjLajDT4Ummqb6t9X40pdODDdq6Uo',
+    authDomain: 'ngrx-school.firebaseapp.com',
+    projectId: 'ngrx-school',
+    storageBucket: 'undefined',
+    messagingSenderId: 'undefined',
+    appId: '1:1042588793805:web:78bd6844eaf9687cb52284',
+    measurementId: 'G-WWP7TYEN8T',
 };
